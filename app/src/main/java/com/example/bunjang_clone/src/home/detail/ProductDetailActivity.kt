ee7d81112c6e32cfc4c541d86dfbbec17fa0c9a9
YunJaem06 <yunjae0616@gmail.com>
2022-08-28 23:28:09 +0900

    Bunjang clone 결제페이지 layout 50%구현

--- app/src/main/java/com/example/bunjang_clone/src/home/detail/ProductDetailActivity.kt
+++ app/src/main/java/com/example/bunjang_clone/src/home/detail/ProductDetailActivity.kt
@@ -3,25 +3,35 @@ package com.example.bunjang_clone.src.home.detail
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.viewpager2.widget.ViewPager2
+import com.example.bunjang_clone.R
 import com.example.bunjang_clone.config.BaseActivity
 import com.example.bunjang_clone.databinding.ActivityProductDetailBinding
 import com.example.bunjang_clone.src.home.detail.models.DetailResponse
 import com.example.bunjang_clone.src.home.detail.models.DetailResult
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import java.text.DecimalFormat
 
-class ProductDetailActivity() : BaseActivity<ActivityProductDetailBinding>(ActivityProductDetailBinding::inflate), DetailActivityInterface {
+class ProductDetailActivity() : BaseActivity<ActivityProductDetailBinding>(ActivityProductDetailBinding::inflate), DetailActivityInterface,
+    View.OnClickListener {
 
     var productIdx = 0
     private var prdouctPageCnt = 0
 
-    private lateinit var tagAdapter : DetailTagAdapter
+    var delivery = false
+    var meet = false
+    var deliverybtn = false
 
+    private lateinit var tagAdapter : DetailTagAdapter
     private lateinit var imgAdapter : ProductSliderAdapter
 
-    var vpImgSlider = mutableListOf<String>()
+    private lateinit var chargeDialog: BottomSheetDialog
 
     private lateinit var items: DetailResult
+
     val priceFormat = DecimalFormat("#,###")
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,22 +39,10 @@ class ProductDetailActivity() : BaseActivity<ActivityProductDetailBinding>(Activ
 
         productIdx = intent.getIntExtra("productIdx", 0)
         getDetailData(productIdx)
-    }
 
-//    private fun imgViewPage() {
-//        binding.vpProduct.adapter = imgSliderAdapter
-//
-//        binding.vpProduct.orientation = ViewPager2.ORIENTATION_HORIZONTAL
-//
-//        binding.vpProduct.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-//            override fun onPageSelected(position: Int) {
-//                binding.tvProductPage.text = "${binding.vpProduct.currentItem + 1}/${}"
-//
-//                adPageCnt = binding.vpHomeAd.currentItem
-//                super.onPageSelected(position)
-//            }
-//        })
-//    }
+        binding.btnProductCharge.setOnClickListener(this)
+
+    }
 
     private fun getDetailData(productIdx: Int) {
         DetailService(this).detailGetData(productIdx)
@@ -95,5 +93,31 @@ class ProductDetailActivity() : BaseActivity<ActivityProductDetailBinding>(Activ
 
     override fun onGetDetailDataFail(message: String) {
 
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id) {
+            R.id.btn_product_charge -> {
+                val chargeView = layoutInflater.inflate(R.layout.dialog_detail_product_buy, null)
+                chargeDialog = BottomSheetDialog(this)
+                chargeDialog.setContentView(chargeView)
+                chargeDialog.show()
+
+
+                chargeView.findViewById<ConstraintLayout>(R.id.cl_product_delivery).setOnClickListener {
+                    if (delivery) {
+                        chargeView.findViewById<ImageView>(R.id.iv_product_deal_click).setImageResource(R.drawable.icon_radio_unclick)
+                        chargeView.findViewById<ConstraintLayout>(R.id.cl_product_delivery).setBackgroundColor(R.drawable.btn_product_red)
+                        delivery = false
+                        deliverybtn = false
+                    } else {
+                        chargeView.findViewById<ImageView>(R.id.iv_product_deal_click).setImageResource(R.drawable.icon_radio_click)
+                        chargeView.findViewById<ConstraintLayout>(R.id.cl_product_delivery).setBackgroundColor(R.drawable.btn_product_event)
+                        delivery = true
+                        deliverybtn = true
+                    }
+                }
+            }
+        }
     }
 }
