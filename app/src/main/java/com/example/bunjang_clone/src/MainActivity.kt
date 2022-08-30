8544d0405ec29a7afe2ef019d67c26ede6552071
YunJaem06 <yunjae0616@gmail.com>
2022-08-30 22:32:58 +0900

    Bunjang clone 등록페이지 dialog 완료

--- app/src/main/java/com/example/bunjang_clone/src/MainActivity.kt
+++ app/src/main/java/com/example/bunjang_clone/src/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.bunjang_clone.src
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.example.bunjang_clone.R
@@ -7,6 +9,7 @@ import com.example.bunjang_clone.config.BaseActivity
 import com.example.bunjang_clone.databinding.ActivityMainBinding
 import com.example.bunjang_clone.src.home.HomeFragment
 import com.example.bunjang_clone.src.mypage.MyPageFragment
+import com.example.bunjang_clone.src.register.RegisterActivity
 import com.example.bunjang_clone.src.register.RegisterFragment
 import com.example.bunjang_clone.src.search.SearchFragment
 import com.example.bunjang_clone.src.talk.TalkFragment
@@ -19,20 +22,34 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
         supportFragmentManager.beginTransaction().add(R.id.fl_main_page, HomeFragment()).commit()
 
         binding.mainBtnNav.setOnItemSelectedListener {
-            replaceFragment(
-                when(it.itemId){
-                    R.id.main_nav_home -> HomeFragment()
-                    R.id.main_nav_search -> SearchFragment()
-                    R.id.main_nav_register -> RegisterFragment()
-                    R.id.main_nav_talk -> TalkFragment()
-                    else -> MyPageFragment()
+            when (it.itemId) {
+                R.id.main_nav_home -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fl_main_page, HomeFragment())
+                        .commit()
                 }
-            )
+                R.id.main_nav_search -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fl_main_page, SearchFragment())
+                        .commit()
+                }
+                R.id.main_nav_register -> {
+                    startActivity(Intent(this, RegisterActivity::class.java))
+                    return@setOnItemSelectedListener false
+                }
+                R.id.main_nav_talk -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fl_main_page, TalkFragment())
+                        .commit()
+                }
+                else -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fl_main_page, MyPageFragment())
+                        .commit()
+                }
+            }
             true
         }
-    }
 
-    private fun replaceFragment(fragment: Fragment){
-        supportFragmentManager.beginTransaction().replace(R.id.fl_main_page, fragment).commit()
     }
 }
---
+++ app/src/main/java/com/example/bunjang_clone/src/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bunjang_clone.src.register
+
+import android.os.Bundle
+import com.example.bunjang_clone.config.BaseActivity
+import com.example.bunjang_clone.databinding.ActivityRegisterBinding
+
+class RegisterActivity : BaseActivity<ActivityRegisterBinding>(ActivityRegisterBinding::inflate) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
